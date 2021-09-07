2579f232086e86fbf273999ea557fa1fb226423b
Will St. Onge <ongewill@gmail.com>
2021-09-07 09:15:13 -0400

    Final changes

--- app/src/main/java/com/csc415/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/csc415/helloworldapp/MainActivity.kt
@@ -2,12 +2,18 @@ package com.csc415.helloworldapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity()
 {
+	private val LOG_TAG = MainActivity::class.java.simpleName
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		Log.d(LOG_TAG, "Hello World");
+		Log.e(LOG_TAG, "Happy Birthday", IllegalArgumentException())
 	}
 }
